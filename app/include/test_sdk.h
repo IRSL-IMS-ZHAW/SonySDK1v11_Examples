0e53a77f40fc737ee675213b61190185d983a654
Charith Munasinghe <mung@zhaw.ch>
2024-06-05 18:40:08 +0200

    Initial commit
    refactoring example app and code from sony
    adding executable for testing
    chages to cmakefile

---
+++ app/include/test_sdk.h
@@ -0,0 +1,44 @@
+#ifndef MY_SONY_SDK_H
+#define MY_SONY_SDK_H
+
+#include <CameraRemote_SDK.h>
+#include <CrDeviceProperty.h>
+#include <CrTypes.h>
+#include <CrCommandData.h>
+#include <CrError.h>
+#include <ICrCameraObjectInfo.h>
+#include <CrDefines.h>
+#include <CrImageDataBlock.h>
+#include <IDeviceCallback.h>
+
+#include <ConnectionInfo.h>
+#include <MessageDefine.h>
+#include <CameraDevice.h>
+#include <PropertyValueTable.h>
+#include <Text.h>
+
+#include <iostream>
+
+// function declaration
+bool Init();
+void Terminate();
+void Disconnect(SCRSDK::CrDeviceHandle);
+bool ConnectCamera(SCRSDK::ICrCameraObjectInfo *pcamera);
+SCRSDK::ICrCameraObjectInfo* CreateUSBObject();
+
+/*
+class MyDeviceCallback : public SCRSDK::IDeviceCallback {
+    void OnConnected(SCRSDK::DeviceConnectionVersioin version) override;
+    void OnDisconnected(CrInt32u error) override;
+    void OnPropertyChanged() override;
+    void OnPropertyChangedCodes(CrInt32u num, CrInt32u* codes) override;
+    void OnLvPropertyChanged() override;
+    void OnLvPropertyChangedCodes(CrInt32u num, CrInt32u* codes) override;
+    void OnCompleteDownload(CrChar* filename, CrInt32u type) override ;
+    void OnNotifyContentsTransfer(CrInt32u notify, SCRSDK::CrContentHandle handle, CrChar* filename) override;
+    void OnWarning(CrInt32u warning) override ;
+    void OnWarningExt(CrInt32u warning, CrInt32 param1, CrInt32 param2, CrInt32 param3) override;
+    void OnError(CrInt32u error) override;
+};
+*/
+#endif // MY_SONY_SDK_H
